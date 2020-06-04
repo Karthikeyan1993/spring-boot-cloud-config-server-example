6fade7ffba3ed36f003c61478dcd7084dff83ed4
Karthikeyan1993 <karthik.infotec99@gmail.com>
2020-06-04 23:15:01 +0530

    config changes

--- src/main/java/com/karthik/springbootcloudconfigserver/SpringBootCloudConfigServerApplication.java
+++ src/main/java/com/karthik/springbootcloudconfigserver/SpringBootCloudConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.karthik.springbootcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringBootCloudConfigServerApplication {
 
 	public static void main(String[] args) {
